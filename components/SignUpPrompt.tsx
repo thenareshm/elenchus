fb2765b04ecf9d8ebddb7a63ccf74b7f869f8a9f
Your Name <thenareshm4@gmail.com>
2025-07-11 18:28:24 -0500

    changes

--- components/SignUpPrompt.tsx
+++ components/SignUpPrompt.tsx
@@ -1,36 +1,86 @@
 "use client"
 
-import React from 'react'
+import React, { useRef } from 'react'
 import SignUpModal from './modals/SignUpModal'
 import LogInModal from './modals/LogInModal'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '@/redux/store'
+import HeartbrainIcon from './icons/HeartbrainIcon'
+import { openLogInModal, openSignUpModal } from '@/redux/slices/modalSlice'
 
 export default function SignUpPrompt() {
-  const name = useSelector(( state : RootState) => state.user.username)
-  console.log(name)
-  
+  const username = useSelector((state: RootState) => state.user.username)
+  const dispatch = useDispatch()
+
+  const clickAudioRef = useRef<HTMLAudioElement | null>(null)
+  const hoverAudioRef = useRef<HTMLAudioElement | null>(null)
+
+  const playClickSound = () => {
+    if (clickAudioRef.current) {
+      clickAudioRef.current.currentTime = 0
+      clickAudioRef.current.play()
+    }
+  }
+
+  const playHoverSound = () => {
+    if (hoverAudioRef.current) {
+      hoverAudioRef.current.currentTime = 0
+      hoverAudioRef.current.play()
+    }
+  }
+
+  if (username) return null
+
   return (
-    !name &&
-    <div
-      id="signup-prompt"
-      className='fixed w-full h-[80px] bg-[#C0BAB5]
-      bottom-0 flex justify-center items-center md:space-x-5
-      lg:justify-between lg:px-20 xl:px-40 2xl:px-80 z-[100]'
-    >
-    
-        <div className='hidden md:flex flex-col text-white'>
-            <span className='text-xl font bold'>Don't miss out on being sensible</span>
-            <span>People on Sensebook are always making sense</span>
-        </div>
-    
-        <div className='flex space-x-2 w-full md:w-fit p-3'>
-            <LogInModal />
-            
-            <SignUpModal />
-        </div>
-    
-    
+    <div id="signup-prompt" className="fixed bottom-6 right-10 z-[100] group">
+      <div
+        className="relative w-[170px] h-[170px] bg-[#C0BAB5] rounded-full shadow-2xl 
+        flex items-center justify-center transition-transform hover:scale-105 cursor-pointer"
+      >
+        {/* Log In Button (left) */}
+        <button
+          onClick={() => {
+            playClickSound()
+            dispatch(openLogInModal())
+          }}
+          onMouseEnter={playHoverSound}
+          title="Log In"
+          className="absolute left-0 top-0 w-1/2 h-full rounded-l-full z-20 
+          text-sm font-bold text-black hover:bg-white/10 transition pr-2 flex items-center justify-center"
+        >
+          Log In
+        </button>
+
+        {/* Sign Up Button (right) */}
+        <button
+          onClick={() => {
+            playClickSound()
+            dispatch(openSignUpModal())
+          }}
+          onMouseEnter={playHoverSound}
+          title="Sign Up"
+          className="absolute right-0 top-0 w-1/2 h-full rounded-r-full z-20 
+          text-sm font-bold text-black hover:bg-white/10 transition pl-4 flex items-center justify-center"
+        >
+          Sign Up
+        </button>
+
+        {/* Pulsing HeartBrain Icon */}
+        <HeartbrainIcon
+          className="w-12 h-12 top-[46px] text-white pointer-events-none animate-pulse-sense"
+          title="Join Sensebook"
+        />
+      </div>
+
+      {/* Keep modals mounted */}
+      <div className="hidden">
+        <LogInModal />
+        <SignUpModal />
+      </div>
+
+      {/* Audio Elements */}
+      <audio ref={clickAudioRef} src="/sounds/touchpad.mp3" preload="auto" />
+      <audio ref={hoverAudioRef} src="/sounds/hoversound400.mp3" preload="auto" />
     </div>
   )
 }
