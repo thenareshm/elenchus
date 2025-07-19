3902d968f828fd4afe90030d4f0215151705b23b
Your Name <thenareshm4@gmail.com>
2025-07-19 00:03:14 -0500

    loginchange

--- app/page.tsx
+++ app/page.tsx
@@ -136,7 +136,7 @@ export default function Home() {
       <LoadingScreen />
       <Footer />
 
-       { /* {showOnboarding && <WebsiteOnboarding onComplete={handleOnboardingComplete} />} */}
+       {showOnboarding && <WebsiteOnboarding onComplete={handleOnboardingComplete} />}
     </>
   );
 }
